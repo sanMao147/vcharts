0a256dbe035a729e1372b472f1f5b12911ae5688
wohool <1529063213@qq.com>
2023-04-03 19:18:16 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
 set -e
+###
+ # @Author: wohool
+ # @Date: 2023-04-03 17:39:52
+ # @LastEditors: wohool
+ # @LastEditTime: 2023-04-03 19:17:29
+ # @FilePath: \webgis-demo\deploy.sh
+ # @Description: 自动化部署GitHub Pages
+ # 
+ # Copyright (c) 2023 by sanMao147----charlielin007@outlook.com, All Rights Reserved. 
+### 
 
+yarn build
 
-yarn docs:build
+cd dist
+
 git add -A
 git commit -m 'deploy'
-git push -u origin main
+# git push -u origin main
 git subtree push --prefix dist origin gh-pages
-
-
 
 cd -
